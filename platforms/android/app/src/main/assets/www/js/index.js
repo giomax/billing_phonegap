c14da19aa8e3f284401cc63fd2a3b92b93d36c77
Giorgi Kvavilashvili <Giorgi.Kvavilashvili@uag.local>
2020-07-08 12:45:44 +0400

    Changed logo

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,7 +1,8 @@
-
-	
-
 var app = {
+	debug:function(text){
+		console.log(text);
+		$('.debug').html(text);
+	},
     initialize: function() {
         this.bindEvents();
     },    
@@ -12,14 +13,9 @@ var app = {
         app.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-		console.log("------");
-		console.log(id);
-		console.log("------");
-        
-		
-		
+		var base_url = "http://billing.com.ge/login";
 		var onSuccess = function(position) {
-			console.log('Latitude: '          + position.coords.latitude          + '\n' +
+			app.debug('Latitude: '          + position.coords.latitude          + '\n' +
 				  'Longitude: '         + position.coords.longitude         + '\n' +
 				  'Altitude: '          + position.coords.altitude          + '\n' +
 				  'Accuracy: '          + position.coords.accuracy          + '\n' +
@@ -27,30 +23,43 @@ var app = {
 				  'Heading: '           + position.coords.heading           + '\n' +
 				  'Speed: '             + position.coords.speed             + '\n' +
 				  'Timestamp: '         + position.timestamp                + '\n');	
-
-
-			  $('body').append('Latitude: '          + position.coords.latitude          + '\n' +
-				  'Longitude: '         + position.coords.longitude         + '\n' +
-				  'Altitude: '          + position.coords.altitude          + '\n' +
-				  'Accuracy: '          + position.coords.accuracy          + '\n' +
-				  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-				  'Heading: '           + position.coords.heading           + '\n' +
-				  'Speed: '             + position.coords.speed             + '\n' +
-				  'Timestamp: '         + position.timestamp                + '\n');
-				  
 		};
-
-		// onError Callback receives a PositionError object
-		//
 		function onError(error) {
-			$('body').append('code: '    + error.code    + '\n' +
+			app.debug('code: '    + error.code    + '\n' +
 				  'message: ' + error.message + '\n');
 		}
-
 		navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-
-			
+		app.debug('shemovida');
+		  
+		$(document).on('submit', '#login', function(e){
+			var $btn = $("#login [type='submit']");
+			$btn.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> იტვირთება...').prop('disabled',true);
+			e.preventDefault();
+				$.ajax({
+					  method: "POST",
+					  url: base_url+'login',
+					  data:{
+						  'email':$("[name='email']").val(),
+						  'password':$("[name='password']").val(),
+						  'captcha':$("[name='captcha']").val(),
+					  }
+					}).done(function(data){
+						
+						if(data.code == 0){
+							swal(data.text);
+							$('.capchaform').html(data.capcha+'<i class="fas fa-sync" style="margin-left:10px;cursor:pointer;"></i>');
+							$('[name="captcha"]').val('');
+							$btn.prop('disabled',false).html("ავტორიზაცია");
+						}
+						if(data.code == 1){
+							window.location.href = base_url;
+						}
+						if(data.code == 2){
+							window.location.href = base_url+"password_reset";
+						}
+					});
+		});
+		
 		
     }
 };
