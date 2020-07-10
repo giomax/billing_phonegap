5bc31db208ac8bf457f8776d1df02d13d6ab299d
Giorgi Kvavilashvili <Giorgi.Kvavilashvili@uag.local>
2020-07-10 15:40:47 +0400

    gps tracking test

--- www/js/index.js
+++ www/js/index.js
@@ -250,23 +250,6 @@ var app = {
 							$('.modal:visible').modal('hide').remove();
 							$('body').append("<div class='gis_map'></div>");
 							$('.gis_map').html(data);
-							
-							var onSuccess = function(position) {
-								app.debug('Latitude: '          + position.coords.latitude          + '\n' +
-									  'Longitude: '         + position.coords.longitude         + '\n' +
-									  'Altitude: '          + position.coords.altitude          + '\n' +
-									  'Accuracy: '          + position.coords.accuracy          + '\n' +
-									  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-									  'Heading: '           + position.coords.heading           + '\n' +
-									  'Speed: '             + position.coords.speed             + '\n' +
-									  'Timestamp: '         + position.timestamp                + '\n');	
-							};
-							function onError(error) {
-								app.debug('code: '    + error.code    + '\n' +
-									  'message: ' + error.message + '\n');
-							}
-							navigator.geolocation.getCurrentPosition(onSuccess, onError);
-							
 					});
 			
 		});
